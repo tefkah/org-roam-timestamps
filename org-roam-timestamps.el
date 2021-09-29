8bf0caa85ae6fc509e41cd2429994ab90a8f9d71
Thomas F. K. Jorna <hello@tefkah.com>
2021-09-29 15:41:12 +0200

    basic timestamp-all function

--- org-roam-timestamps.el
+++ org-roam-timestamps.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: org-roam zettelkasten time
 ;; Homepage: https://github.com/thomas/org-roam-timestamps
-;; Package-Requires: ((emacs "26.1"))
+;; Package-Requires: ((emacs "26.1") org-roam)
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -18,6 +18,8 @@
 ;;
 ;;
 ;;; Code:
+
+(require 'org-roam)
 
 (defgroup org-roam-timestamps nil
   "Creation and modification timestamps in Org-roam."
@@ -35,10 +37,39 @@
   (when org-roam-timestamps-mode
     (message "Stamping")))
 
+(defun org-roam-timestamps-decode (mtime)
+  "Decode a list of seconds since 1970 MTIME to an org-roam-timestamp."
+  (let ((time (decode-time mtime))
+        dec-time
+        el)
+  (dotimes (i 6 )
+    (setq el (number-to-string (nth i time)))
+      (when (length= el 1) (setq el (concat "0" el)))
+    (setq dec-time (concat
+                    el
+                    dec-time)))
+  dec-time))
 
 (defun org-roam-timestamps-all ()
   "Go through all nodes and add timestamps to them."
-  (message "stampin"))
+  (interactive)
+  (let ((nodes (org-roam-db-query [:select id :from nodes])))
+    (dolist (node nodes)
+      (let* ((n (org-roam-populate (org-roam-node-create :id (car node))))
+             (file (org-roam-node-file n))
+             (mtime (org-roam-timestamps-decode (org-roam-node-file-mtime n)))
+             (pos (org-roam-node-point n))
+             (level (org-roam-node-level n))
+             (title (org-roam-node-title n)))
+            (org-roam-with-file file nil
+              (goto-char pos)
+              (org-roam-add-property mtime "mtime")
+                  (if-let ((filename (file-name-base file))
+                           (index (string-match "^[0-9]\\{14\\}" filename))
+                           (timestamp (substring filename index (+ index 14))))
+                      (org-roam-add-property timestamp "ctime")
+                    (org-roam-add-property mtime "ctime"))
+                  (save-buffer))))))
 
 (provide 'org-roam-timestamps)
 ;;; org-roam-timestamps.el ends here
