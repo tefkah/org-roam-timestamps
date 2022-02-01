5dc82c81b761c89933b4baaf9e655ef40104126b
Sam Stites <stites@users.noreply.github.com>
2022-02-01 08:37:21 -0500

    also fix add-mtime when starting a new initial buffer

--- org-roam-timestamps.el
+++ org-roam-timestamps.el
@@ -103,7 +103,7 @@ Defaults to one hour."
 Optionally checks the minimum time interval you want between mod times
 if you supply the current MTIME."
   (org-with-wide-buffer
-   (let ((pos (org-roam-node-point node))
+   (let ((pos (if node (org-roam-node-point node) (point-marker)))
          (curr (org-roam-timestamps-decode (current-time))))
      (if (and org-roam-timestamps-remember-timestamps mtime)
          (when (> (org-roam-timestamps-subtract curr mtime t) org-roam-timestamps-minimum-gap)
